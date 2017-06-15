f77696bc0350e8854527c1ef46133ff5f06ef48a
Adam Ahrens <drunkengranite@gmail.com>
2017-06-14 18:37:33 -0700

    cleaned up

--- src/components/bodies/Home.js
+++ src/components/bodies/Home.js
@@ -8,13 +8,13 @@ import React from "react";
 import {connect} from "react-redux";
 import Footer from "../Footer";
 import Card from "../Card";
-import MenuItem from '../MenuItem';
-import img1 from './EX3i8.jpg'
-import img2 from './about-image.png'
-import Slider from 'react-slick'
+import MenuItem from "../MenuItem";
+import img1 from "./EX3i8.jpg";
+import img2 from "./about-image.png";
+import Slider from "react-slick";
 
 const Home = (props) => {
-	var settings = {
+	let settings = {
 		dots: false,
 		infinite: true,
 		speed: 5,
@@ -27,18 +27,38 @@ const Home = (props) => {
 
 	return (
 		<div id="body">
-				<Slider {...settings}>
-					<div id="home-item">
-						<img src={img2}/>
-					</div>
-					<div id="home-item">
-						<img src={img1}/>
-					</div>
-				</Slider>
+			<Slider {...settings}>
+				<div id="home-item">
+					<img src={img2}/>
+				</div>
+				<div id="home-item">
+					<img src={img1}/>
+				</div>
+			</Slider>
+			<div className="section">
+				Featured Menu Items
+			</div>
+			<div className="featured-item-container">
+				{
+					props.featuredItems.map((item) => {
+						return (
+							<div className="featured-item">
+								<div className="image">
+									<img src={img1} />
+								</div>
+								<div className="meta">
+									<p className="font-header-1">{item.title}</p>
+									<p className="font-caption">{item.price}</p>
+									<p className="font-body-copy">{item.description}</p>
+								</div>
+							</div>
+						)
+					})
+				}
+			</div>
 		</div>
 	)
 };
-
 
 export default connect(
 	(state) => ({
